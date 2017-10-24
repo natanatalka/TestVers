b49d7b330f180ce7d08c8f401eac7943372ba056
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 17:55:18 +0300

    [Studio] Add screen "Blank2"

--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,3 +1,4 @@
 export * from './Blank/index';
 export * from './Blank1/index';
+export * from './Blank2/index';
 export * from './Login/index';
---
+++ client/src/pages/Blank2/PhonePortrait/Blank2.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-blank2-phoneportrait',
+  templateUrl: 'Blank2.html'
+})
+export class Blank2_PhonePortrait extends Screen {
+}
