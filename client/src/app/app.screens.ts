2e20afff5ec74f559cdfa3a3bb38ce66d5f1882d
test <test@test.com>
2017-10-24 17:38:45 +0300

    [Studio] Add screen "Login"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,14 +2,19 @@
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { Blank_PhonePortrait } from '../pages/Blank/PhonePortrait/Blank';
+import { Login_PhonePortrait } from '../pages/Login/PhonePortrait/Login';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
-    Blank_PhonePortrait
+    Blank_PhonePortrait,
+    Login_PhonePortrait
   ];
   static mapping = {
     'Blank': {
       PhonePortrait: Blank_PhonePortrait
+    },
+    'Login': {
+      PhonePortrait: Login_PhonePortrait
     }
   }
 }
