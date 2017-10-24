47ea9719c9a95c39f0df8d9eef4a0d84e08023a5
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 18:09:07 +0300

    [Studio] Delete screen "Blank"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,22 +1,17 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
-import { Blank_PhonePortrait } from '../pages/Blank/PhonePortrait/Blank';
 import { Login_PhonePortrait } from '../pages/Login/PhonePortrait/Login';
 import { Blank1_PhonePortrait } from '../pages/Blank1/PhonePortrait/Blank1';
 import { Blank3_PhonePortrait } from '../pages/Blank3/PhonePortrait/Blank3';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
-    Blank_PhonePortrait,
     Login_PhonePortrait,
     Blank1_PhonePortrait,
     Blank3_PhonePortrait
   ];
   static mapping = {
-    'Blank': {
-      PhonePortrait: Blank_PhonePortrait
-    },
     'Login': {
       PhonePortrait: Login_PhonePortrait
     },
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,4 +1,3 @@
-export * from './Blank/index';
 export * from './Blank1/index';
 export * from './Blank3/index';
 export * from './Login/index';
